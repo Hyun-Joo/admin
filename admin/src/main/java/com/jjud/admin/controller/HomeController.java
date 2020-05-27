a3f1aea70ee1d6f1253e923c8a1fe0a89250b541
HYUNJOO <HYUNJOO@DESKTOP-BJ080PU>
2020-05-27 20:46:38 +0900

    jjud mansei

--- admin/src/main/java/com/jjud/admin/controller/HomeController.java
+++ admin/src/main/java/com/jjud/admin/controller/HomeController.java
@@ -3,11 +3,15 @@ package com.jjud.admin.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
+		log.info("jjud mansei");
 		return "너는 쥬드하다";
 	}
 
